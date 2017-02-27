9d3be69ec4c1d3d8f41dffbf777c8aa8cfb5df9d
HP-dufeng <fengdu_xiangyang@163.com>
2017-02-27 17:10:22 +0800

    complete backend

--- socket.io/index.js
+++ socket.io/index.js
@@ -22,8 +22,10 @@ const socketAuth = (socket, next) => {
 
     redisSession.get(sid, (err, session) => {
         if(session.isAuthenticated){
-            socket.user = session.user;
-            socket.sid = sid;
+            socket.request.user = session.passport.user;
+			socket.request.sid = sid;
+			redisChat.addUser(session.passport.user.id, session.passport.user.displayName, session.passport.user.provider);
+			
             return next();
         } else {
             return next(new Error('Not Authenticated'));
@@ -31,14 +33,97 @@ const socketAuth = (socket, next) => {
     });
 };
 
+const removeFromRoom = (socket, room) => {
+	socket.leave(room);
+	redisChat.removeUserFromRoom(socket.request.user.id, room);
+	socket.broadcast.to(room).emit('RemoveUser',
+		models.User(socket.request.user.id, socket.request.user.displayName, socket.request.user.provider));
+};
+
+const removeAllRooms = (socket, cb) => {
+	const current = socket.rooms;
+	const len = Object.keys(current).length;
+	let i = 0;
+	for(var r in current)
+	{
+		if (current[r] !== socket.id)
+		{
+			removeFromRoom(socket, current[r]);
+		}
+		i++;
+		if (i === len) cb();
+
+	}
+};
+
+
 const socketConnection = socket => {
-    socket.on('GetMe', function(){});
-    socket.on('GetUser', function(room){});
-    socket.on('GetChat', function(data){});
-    socket.on('AddChat', function(chat){});
-    socket.on('GetRoom', function(){});
-    socket.on('AddRoom', function(r){});
-    socket.on('disconnect', function(){});
+    socket.on('GetMe', () => {
+        const { id, diaplayName, provider } = socket.request.user;
+        socket.emit('GetMe', models.User(id, displayName, provider));
+    });
+    
+    socket.on('GetUser', function(room){
+		var  usersP = redisChat.getUsersinRoom(room.room);
+		usersP.done(function(users){
+			socket.emit('GetUser', users);
+		});
+	});
+
+	socket.on('GetChat', function(data){
+		redisChat.getChat(data.room, function(chats){
+			var retArray = [];
+			var len = chats.length;
+			chats.forEach(function(c){
+				try{
+					retArray.push(JSON.parse(c));
+				}catch(e){
+					log.error(e.message);
+				}
+				len--;
+				if (len === 0) socket.emit('GetChat', retArray);
+			});
+		});
+	});
+
+	socket.on('AddChat', function(chat){
+		var newChat = models.Chat(chat.message, chat.room,
+			models.User(socket.request.user.id, socket.request.user.displayName, socket.request.user.provider));
+		redisChat.addChat(newChat);
+		socket.broadcast.to(chat.room).emit('AddChat', newChat);
+		socket.emit('AddChat', newChat);
+	});
+
+	socket.on('GetRoom', function(){
+		redisChat.getRooms(function(rooms){
+			var retArray = [];
+			var len = rooms.length;
+			rooms.forEach(function(r){
+				retArray.push(models.Room(r));
+				len--;
+				if(len === 0) socket.emit('GetRoom', retArray);
+			});
+		});
+	});
+
+	socket.on('AddRoom', function(r){
+		var room = r.name;
+		removeAllRooms(socket, function(){
+			if (room !== '')
+			{
+				socket.join(room);
+				redisChat.addRoom(room);
+				socket.broadcast.emit('AddRoom', models.Room(room));
+				socket.broadcast.to(room).emit('AddUser',
+					models.User(socket.request.user.id, socket.request.user.displayName, socket.request.user.provider));
+				redisChat.addUserToRoom(socket.request.user.id, room);
+			}
+		});
+	});
+
+	socket.on('disconnect', function(){
+        removeAllRooms(socket, function(){});
+	});
 };
 
 exports.startIo = server => {
